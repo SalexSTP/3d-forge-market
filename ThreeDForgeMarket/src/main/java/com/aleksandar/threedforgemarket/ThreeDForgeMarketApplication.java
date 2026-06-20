6d1886a6505a4d8eed98887fb6d597461a73fbb0
Aleksandar-Stoyanov <135261672+SalexSTP@users.noreply.github.com>
2026-06-20 18:03:00 +0300

    chore: initialize Spring Boot project

---
+++ ThreeDForgeMarket/src/main/java/com/aleksandar/threedforgemarket/ThreeDForgeMarketApplication.java
@@ -0,0 +1,13 @@
+package com.aleksandar.threedforgemarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreeDForgeMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreeDForgeMarketApplication.class, args);
+    }
+
+}
---
+++ ThreeDForgeMarket/src/test/java/com/aleksandar/threedforgemarket/ThreeDForgeMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aleksandar.threedforgemarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreeDForgeMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
